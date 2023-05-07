f6281927e3a94136e80c5cf2188026759f4512c0
Andres <andrsbtrg@gmail.com>
2023-05-07 17:32:50 +0200

    Add camera

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
+
+pub const SIDE_PANEL_DEFAULT_WIDTH: f32 = 200.;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
         .init_resource::<OccupiedScreenSpace>()
+        .add_startup_system(setup_scene)
         .add_startup_system(spawn_camera)
         .add_system(ui_system)
         .run();
 }
-pub const SIDE_PANEL_DEFAULT_WIDTH: f32 = 200.;
-
-#[derive(Component)]
-pub struct MyCamera;
 
 #[derive(Default, Resource)]
 pub struct OccupiedScreenSpace {
@@ -21,18 +20,6 @@ pub struct OccupiedScreenSpace {
     _top: f32,
     _right: f32,
     _bottom: f32,
-}
-
-pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
-    let window = window_query.get_single().unwrap();
-
-    commands.spawn((
-        Camera2dBundle {
-            transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.),
-            ..default()
-        },
-        MyCamera,
-    ));
 }
 
 fn ui_system(mut contexts: EguiContexts, mut occupied_screen_space: ResMut<OccupiedScreenSpace>) {
@@ -47,4 +34,73 @@ fn ui_system(mut contexts: EguiContexts, mut occupied_screen_space: ResMut<Occup
         .response
         .rect
         .width();
+}
+
+/// Tags an entity as capable of panning and orbiting.
+#[derive(Component)]
+struct PanOrbitCamera {
+    /// The "focus point" to orbit around. It is automatically updated when panning the camera
+    pub focus: Vec3,
+    pub radius: f32,
+    pub upside_down: bool,
+}
+
+impl Default for PanOrbitCamera {
+    fn default() -> Self {
+        PanOrbitCamera {
+            focus: Vec3::ZERO,
+            radius: 5.0,
+            upside_down: false,
+        }
+    }
+}
+
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Ground plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane {
+            size: 50.0,
+            subdivisions: 0,
+        })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+    // Cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+    // Light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
+}
+
+/// Spawn a camera like this
+fn spawn_camera(mut commands: Commands) {
+    let translation = Vec3::new(-2.0, 2.5, 5.0);
+    let radius = translation.length();
+
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        },
+        PanOrbitCamera {
+            radius,
+            ..Default::default()
+        },
+    ));
 }

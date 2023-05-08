de85dc1c085ac718acc4dd54d9c4a18e10a63f30
Andres <andrsbtrg@gmail.com>
2023-05-08 19:12:41 +0200

    Add toggle scale with '/'

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,5 @@
-use bevy::prelude::{Plugin, ResMut, Resource};
-use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use bevy::{prelude::*, window::PrimaryWindow};
+use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiSettings};
 
 pub const SIDE_PANEL_DEFAULT_WIDTH: f32 = 200.;
 
@@ -9,7 +9,8 @@ impl Plugin for UiPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_plugin(EguiPlugin)
             .init_resource::<OccupiedScreenSpace>()
-            .add_system(ui_system);
+            .add_system(ui_system)
+            .add_system(update_ui_scale_factor_system);
     }
 }
 
@@ -24,7 +25,23 @@ pub struct OccupiedScreenSpace {
 
 fn ui_system(mut contexts: EguiContexts, mut occupied_screen_space: ResMut<OccupiedScreenSpace>) {
     let ctx = contexts.ctx_mut();
-    occupied_screen_space.right = egui::SidePanel::right("left_panel")
+
+    // Top panel
+    #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
+    egui::TopBottomPanel::top("top_panel")
+        .default_height(20.)
+        .show(ctx, |ui| {
+            ui.menu_button("File", |ui| {
+                if ui.button("Quit").clicked() {
+                    // quit
+                    // ToDo! find a better way to exit
+                    std::process::exit(0);
+                }
+            })
+        });
+
+    // Side panel
+    occupied_screen_space.right = egui::SidePanel::left("left_panel")
         .default_width(SIDE_PANEL_DEFAULT_WIDTH)
         .resizable(true)
         .show(ctx, |ui| {
@@ -34,4 +51,24 @@ fn ui_system(mut contexts: EguiContexts, mut occupied_screen_space: ResMut<Occup
         .response
         .rect
         .width();
+}
+
+fn update_ui_scale_factor_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut toggle_scale_factor: Local<Option<bool>>,
+    mut egui_settings: ResMut<EguiSettings>,
+    windows: Query<&Window, With<PrimaryWindow>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Slash) || toggle_scale_factor.is_none() {
+        *toggle_scale_factor = Some(!toggle_scale_factor.unwrap_or(true));
+
+        if let Ok(window) = windows.get_single() {
+            let scale_factor = if toggle_scale_factor.unwrap() {
+                1.0
+            } else {
+                1.0 / window.scale_factor()
+            };
+            egui_settings.scale_factor = scale_factor;
+        }
+    }
 }
